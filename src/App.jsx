12826ff8e09cc40b907e24956e76bb2f9d269152
shaheer417 <niazi2822@gmail.com>
2024-11-07 22:14:50 +0500

    Quiz App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Quiz from "./Components/Quiz";
+
+function App() {
+  return <Quiz />;
+}
+
+export default App;
